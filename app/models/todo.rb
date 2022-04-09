bfcd2da860fd4bbdc5dd534b88b1960356b233a6
Avishek Jana <avishek@geogo.in>
2022-04-09 22:21:34 +0530

    Todo controller updated

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -10,4 +10,30 @@ class Todo < ApplicationRecord
     due_date == Date.today
   end
 
+  # def self.add_task args
+  #   Todo.create(todo_text: args[:todo_text], due_date: Date.today + args[:due_in_days], completed: false)
+  # end
+
+  def self.mark_as_complete todo_id
+    todo = Todo.find(todo_id)
+    todo.update(completed: true) if todo.present?
+    return todo
+  end
+
+  def self.overdue
+    where(["due_date < ?", Date.today])
+  end
+
+  def self.due_today
+    where(["due_date =?", Date.today])
+  end
+
+  def self.due_later
+    where(["due_date >?", Date.today])
+  end
+
+  def self.completed
+    where(["completed >?", true])
+  end
+
 end
