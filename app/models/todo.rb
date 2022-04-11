ec4739c200ef17c66f3d13d5c65d4500c7fd2b82
Avishek Jana <avishek@geogo.in>
2022-04-11 08:41:12 +0530

    Todo checked implemented

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -33,7 +33,11 @@ class Todo < ApplicationRecord
   end
 
   def self.completed
-    where(["completed >?", true])
+    where(["completed =?", true])
+  end
+
+  def self.incomplete
+    where(["completed =?", false])
   end
 
 end
