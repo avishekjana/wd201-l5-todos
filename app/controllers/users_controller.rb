0788ae6210980705ff44b4779d5cfec1ba567715
Avishek Jana <avishek@geogo.in>
2022-04-12 19:55:03 +0530

    Signup validation edded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,13 @@ class UsersController < ApplicationController
   def create
     email = params[:email]
     password = params[:password]
-    new_user = User.create!(first_name: params[:first_name], last_name: params[:last_name], email: email, password: password)
-    session[:current_user_id] = new_user.id
-    redirect_to "/"
+    new_user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: email, password: password)
+    if new_user.save
+      session[:current_user_id] = new_user.id
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
 end
